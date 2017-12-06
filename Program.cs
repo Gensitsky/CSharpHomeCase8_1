5dafb4bedb32a89ddd265f8c3274490094b9b18f
Gensitsky <vadim.gensitsky@gmail.com>
2017-12-06 08:24:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+    Создать примитивный англо-русский и русско-ан-
+    глийский словарь, содержащий пары слов — названий
+    стран на русском и английском языках. Пользователь
+    должен иметь возможность выбирать направление пе-
+    ревода и запрашивать перевод.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomeCase8_1
+{
+    /// <summary>
+    /// Assignment in work
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
